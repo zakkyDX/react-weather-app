f16e81834aa83f7802dc507d6d25cc81902dafd9
HW <hw@HWnoMacBook-Pro.local>
2023-06-23 22:50:04 -0700

    ハンズオン4完了/エラーハンドリング追加

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,21 +2,50 @@ import { useState } from "react";
 import "./App.css";
 import Title from "./components/Title";
 import Form from "./components/Form";
+import Results from "./components/Results";
+
+type ResultStateType = {
+  country: string;
+  cityName: string;
+  temperature: string;
+  conditionText: string;
+  icon: string;
+};
 
 function App() {
   const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState<ResultStateType>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: "",
+  });
   const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
+    console.log("getWeather");
+    //getWeatherボタンを押した時に画面の再読み込みをしないようにする
     e.preventDefault();
+
     fetch(
       `https://api.weatherapi.com/v1/current.json?key=006cc26591aa4683a8161611232705&q=${city}&aqi=no`
-    ).then((res) => {
-      console.log(res);
-    });
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setResults({
+          country: data.location.country,
+          cityName: data.location.name,
+          temperature: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.condition.icon,
+        });
+      })
+      .catch(() => window.alert("通信に失敗しました"));
   };
   return (
     <div className="apptest">
       <Title />
       <Form getWeather={getWeather} setCity={setCity} />
+      <Results results={results} />
     </div>
   );
 }
--- app/src/components/Results.tsx
+++ app/src/components/Results.tsx
@@ -1,5 +1,33 @@
-const Results = () => {
-  return <></>;
+type ResultPropsType = {
+  results: {
+    country: string;
+    cityName: string;
+    temperature: string;
+    conditionText: string;
+    icon: string;
+  };
+};
+
+const Results = (props: ResultPropsType) => {
+  return (
+    <div>
+      {props.results.country && (
+        <div className="results-country">{props.results.country}</div>
+      )}
+      {props.results.cityName && (
+        <div className="results-city">{props.results.cityName}</div>
+      )}
+      {props.results.temperature && (
+        <div className="results-temp">{props.results.temperature}</div>
+      )}
+      {props.results.conditionText && (
+        <div className="results-condition">
+          <img src={props.results.icon} alt="icon" />
+          <span>{props.results.conditionText}</span>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Results;
